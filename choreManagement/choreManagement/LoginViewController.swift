72a98e82e2458efc41b49ebbcbe8bff2a8c563b1
Jon Brown <jbb3573@zoe.cs.utexas.edu>
2022-10-28 18:19:08 -0500

    adding login functionality

---
+++ choreManagement/choreManagement/LoginViewController.swift
@@ -0,0 +1,95 @@
+//
+//  LoginViewController.swift
+//  choreManagement
+//
+//  Created by Jon Brown on 10/28/22.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userIDField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var confirmPasswordField: UITextField!
+    
+    @IBOutlet weak var confirmLabel: UILabel!
+    
+    @IBOutlet weak var loginButton: UIButton!    
+    @IBOutlet weak var errorMessage: UILabel!
+    
+    @IBOutlet weak var loginSegCtrl: UISegmentedControl!
+    
+    var segString = "Login"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        confirmLabel.text = ""
+        loginButton.setTitle("Sign In", for: .normal)
+        confirmPasswordField.isHidden = true
+        
+        passwordField.isSecureTextEntry = true
+        confirmPasswordField.isSecureTextEntry = true
+
+        Auth.auth().addStateDidChangeListener() {
+            auth, user in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                self.userIDField.text = nil
+                self.passwordField.text = nil
+            }
+        }
+    }
+    
+    @IBAction func loginSegPressed(_ sender: Any) {
+        switch loginSegCtrl.selectedSegmentIndex {
+        case 0:
+            segString = "Login"
+            confirmLabel.text = ""
+            loginButton.setTitle("Sign In", for: .normal)
+            confirmPasswordField.isHidden = true
+        case 1:
+            segString = "Sign Up"
+            confirmLabel.text = "Confirm Password"
+            loginButton.setTitle("Sign Up", for: .normal)
+            confirmPasswordField.isHidden = false
+        default:
+            segString = "Login"
+            confirmLabel.text = "Confirm Password"
+            loginButton.setTitle("Sign Up", for: .normal)
+            confirmPasswordField.isHidden = true
+        }
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+        if (segString == "Login"){
+            Auth.auth().signIn(withEmail: userIDField.text!, password: passwordField.text!) {
+                authResult, error in
+                if let error = error as NSError? {
+                    self.errorMessage.text = "\(error.localizedDescription)"
+                } else {
+                    self.errorMessage.text = ""
+                }
+            }
+        } else {
+            
+            if (passwordField.text == confirmPasswordField.text){
+                Auth.auth().createUser(withEmail: userIDField.text!, password: passwordField.text!) {
+                    authResult, error in
+                    if let error = error as NSError? {
+                        self.errorMessage.text = "\(error.localizedDescription)"
+                    } else {
+                        self.errorMessage.text = ""
+                    }
+                }
+            } else {
+                self.errorMessage.text = "Passwords do not match"
+            }
+            
+        }
+    }
+    
+}
