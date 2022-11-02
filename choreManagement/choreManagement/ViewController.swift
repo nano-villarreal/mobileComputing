8ca9717a9e071bd65e9bdc1527b6b67612b188bb
Jon Brown <jbb3573@zoe.cs.utexas.edu>
2022-11-02 15:30:13 -0500

    adding welcome animation

--- choreManagement/choreManagement/ViewController.swift
+++ choreManagement/choreManagement/ViewController.swift
@@ -11,9 +11,34 @@ import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var visualEffectView: UIVisualEffectView!
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        welcomeLabel.text = ""
+        let titleText = "Welcome"
+        var charIndex = 0.0
+        for letter in titleText {
+            
+            Timer.scheduledTimer(withTimeInterval: 0.32 * charIndex, repeats: false) { (timer) in
+                self.welcomeLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3){
+            self.animateOut()
+        }
+    }
+    
+    func animateOut() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.visualEffectView.effect = nil
+        }) { (success:Bool) in
+            self.visualEffectView.removeFromSuperview()
+        }
     }
     
     @IBAction func logOutPressed(_ sender: Any) {
