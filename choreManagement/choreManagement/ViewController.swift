618c6fe84c092b8f28f7c112f733886a363fbf5e
Vineel Mandava <vineel@VM-Pro.local>
2022-11-28 16:30:48 -0600

    reverse test commit

--- choreManagement/choreManagement/ViewController.swift
+++ choreManagement/choreManagement/ViewController.swift
@@ -36,7 +36,6 @@ class ViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello world")
         welcomeLabel.text = ""
         let titleText = "Welcome"
         var charIndex = 0.0
