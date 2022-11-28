d2425a28086ff5d1592e1db0083c99d0b7de78a5
Vineel Mandava <vineel@VM-Pro.local>
2022-11-28 16:29:44 -0600

    test commit

--- choreManagement/choreManagement/ViewController.swift
+++ choreManagement/choreManagement/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hello world")
         welcomeLabel.text = ""
         let titleText = "Welcome"
         var charIndex = 0.0
